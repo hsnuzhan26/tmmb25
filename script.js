cfa1aa1f2a2efa84294947edf624721bcc108a81
hsnuzhan26 <idbanhusnuzhan01@gmail.com>
2026-03-19 19:09:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,16 +7,14 @@ const downloadBtn = document.getElementById('downloadBtn')
 const template = new Image()
 template.src = 'ucapan.png'
 
-// load font
-const font = new FontFace('CustomFont', 'url(font.ttf)')
-font.load().then(f => {
-  document.fonts.add(f)
-  drawCard('')
+// tunggu gambar + font siap
+Promise.all([
+  document.fonts.ready
+]).then(() => {
+  template.onload = () => {
+    drawCard('')
+  }
 })
-
-template.onload = () => {
-  drawCard('')
-}
 
 input.addEventListener('input', () => {
   drawCard(input.value)
@@ -25,15 +23,12 @@ input.addEventListener('input', () => {
 function drawCard(name) {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  // background
   ctx.drawImage(template, 0, 0, canvas.width, canvas.height)
 
-  // text style
   ctx.fillStyle = '#FFFFFF'
-  ctx.font = '30px Montserrat'
+  ctx.font = '30px "Montserrat", sans-serif'
   ctx.textAlign = 'center'
 
-  // posisi teks
   ctx.fillText(name, canvas.width / 2, canvas.height / 3 + 750)
 }
 
