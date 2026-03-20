544b721dd1aa21eb6e77ed4155b2fab53bd3bd6b
hsnuzhan26 <idbanhusnuzhan01@gmail.com>
2026-03-20 13:56:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,23 @@ const downloadBtn = document.getElementById('downloadBtn')
 const template = new Image()
 template.src = 'ucapan.png'
 
+// load font
 const font = new FontFace('Montserrat', 'url(Montserrat-SemiBold.ttf)')
 font.load().then(f => {
   document.fonts.add(f)
 })
 
-// tunggu gambar + font siap
-Promise.all([
-  document.fonts.ready
-]).then(() => {
-  template.onload = () => {
+// ✅ gambar dulu yang pasti ke-load
+template.onload = () => {
+  // ✅ set ukuran canvas
+  canvas.width = template.width
+  canvas.height = template.height
+
+  // ✅ tunggu font
+  document.fonts.ready.then(() => {
     drawCard('')
-  }
-})
+  })
+}
 
 input.addEventListener('input', () => {
   drawCard(input.value)
@@ -28,18 +32,17 @@ input.addEventListener('input', () => {
 function drawCard(name) {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  ctx.drawImage(template, 0, 0, canvas.width, canvas.height)
+  ctx.drawImage(template, 0, 0)
 
   ctx.fillStyle = '#FFFFFF'
   ctx.font = '60px "Montserrat"'
   ctx.textAlign = 'center'
 
-  ctx.fillText(name, canvas.width / 2, canvas.height / 3 + 750)
+  // ✅ posisi lebih aman
+  ctx.fillText(name, canvas.width / 2, canvas.height * 0.7)
 }
 
 downloadBtn.addEventListener('click', () => {
-  const link = document.createElement('a')
-  link.download = 'idulfitri.png'
-  link.href = canvas.toDataURL('image/png')
-  link.click()
+  // HP friendly
+  window.open(canvas.toDataURL('image/png'))
 })
